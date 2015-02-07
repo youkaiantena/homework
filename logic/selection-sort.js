18073c1da85d4f4440cc44ca441e527cb78cf510
youkaiantena <apple.kitaro@gmail.com>
2015-02-07 20:15:26 +0900

    node.jsで実行して、テスト通った

--- logic/selection-sort.js
+++ logic/selection-sort.js
@@ -1,62 +1,64 @@
-var Sort = (function(){
-  /**
-   * descとは、第1引数と第2引数を比較した際、第2引数が大きい事である
-   *
-   * @param int value
-   * @param int other
-   * @return boolean
-   */
-  function desc(value, other){
-    return (value < other);
-  }
+/**
+ * descとは、第1引数と第2引数を比較した際、第2引数が大きい事である
+ *
+ * @param int value
+ * @param int other
+ * @return boolean
+ */
+function desc(value, other){
+  return (value < other);
+}
 
-  /**
-   * ascとは、第1引数と第2引数を比較した際、第2引数が小さい事である
-   *
-   * @param int value
-   * @param int other
-   * @return boolean
-   */
-  function asc(value, other){
-    return (other < value);
-  }
+/**
+ * ascとは、第1引数と第2引数を比較した際、第2引数が小さい事である
+ *
+ * @param int value
+ * @param int other
+ * @return boolean
+ */
+function asc(value, other){
+  return (other < value);
+}
 
-  /**
-   * selectionSortとは、単純選択法による並び替えを行う事である
-   *
-   * @param array data
-   * @param callback predicate
-   * @return array
-   */
-  function selectionSort(data, predicate){
-    var copy = data.concat();
+/**
+ * selectionSortとは、単純選択法による並び替えを行う事である
+ *
+ * @param array data
+ * @param callback predicate
+ * @return array
+ */
+function selectionSort(data, predicate){
+  var copy = data.concat();
 
-    copy.forEach(function(value, index, _copy){
-      _copy.forEach(function(_value, _index){
-        var valueOfIndex = _copy[index];
+  copy.forEach(function(value, index, _copy){
+    _copy.forEach(function(_value, _index){
+      var valueOfIndex = _copy[index];
 
-        if (index < _index && predicate(valueOfIndex, _value)) {
-          _copy[index] = _value;
-          _copy[_index] = valueOfIndex;
-        }
-      });
+      if (index < _index && predicate(valueOfIndex, _value)) {
+        _copy[index] = _value;
+        _copy[_index] = valueOfIndex;
+      }
     });
+  });
 
-    return copy;
-  }
+  return copy;
+}
 
-  var Sort = {
-    asc: function(data){
+module.privateFuncExports = {
+  desc: desc,
+  asc: asc,
+  selectionSort: selectionSort
+};
+
+module.exports = (function(){
+  function Sort(){
+    this.asc = function(data){
       return selectionSort(data, asc);
-    },
-    desc: function(data){
-      return selectionSort(data, desc);
-    }
-  };
+    };
 
+    this.desc = function(data){
+      return selectionSort(data, desc);
+    };
+  }
   return Sort;
 })();
-
-document.write('asc: ' + Sort.asc([8, 5, 10, 2, 1, 7]));
-document.write('<br>');
-document.write('desc: ' + Sort.desc([8, 5, 10, 2, 1, 7]));
--- test/test.js
+++ test/test.js
@@ -1,17 +1,27 @@
 "use strict";
-var assert = require('power-assert');
-describe('Array', function () {
-    beforeEach(function () {
-        this.ary = [1, 2, 3];
+var requireFrom = require('require-from');
+var assert      = require('power-assert');
+
+describe('単純選択法による昇順、降順のテスト', function(){
+  var privateFuncs = requireFrom('privateFuncExports', module, './../logic/selection-sort');
+  var data         = [2, 9, 3, 5, 6, 10, 1];
+
+  it('ascとは、第1引数と第2引数を比較した際、第2引数が小さい事である', function(){
+    assert(privateFuncs.asc(10, 1) === true);
+  });
+
+  it('descとは、第1引数と第2引数を比較した際、第2引数が大きい事である', function(){
+    assert(privateFuncs.desc(1, 10) === true);
+  });
+
+  describe('selectionSortとは、単純選択法による並び替えを行う事である', function(){
+    it('昇順', function(){
+      var ascResult = [1, 2, 3, 5, 6, 9, 10];
+      assert.deepEqual(privateFuncs.selectionSort(data, privateFuncs.asc), [1, 2, 3, 5, 6, 9, 10]);
     });
-    describe('#indexOf()', function () {
-        it('should return index when the value is present', function () {
-            var zero = 0, two = 2;
-            assert(this.ary.indexOf(zero) === two);
-        });
-        it('should return -1 when the value is not present', function () {
-            var minusOne = -1, two = 2;
-            assert.ok(this.ary.indexOf(two) === minusOne, 'THIS IS AN ASSERTION MESSAGE');
-        });
+    it('降順', function(){
+      var descResult = [10, 9, 6, 5, 3, 2, 1];
+      assert.deepEqual(privateFuncs.selectionSort(data, privateFuncs.desc), [10, 9, 6, 5, 3, 2, 1]);
     });
+  });
 });
